1e9ed6e321e45b035dc0a27388a2f3e65a75cc49
ABINASH KUMAR SHAW <30557422+Abinashshaw@users.noreply.github.com>
2024-12-25 20:39:02 +0530

    Welcome to Smart Electronic Store

---
+++ src/main/java/com/learn/electronic/store/SmartElectronicStoreApplication.java
@@ -0,0 +1,13 @@
+package com.learn.electronic.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartElectronicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartElectronicStoreApplication.class, args);
+	}
+
+}
