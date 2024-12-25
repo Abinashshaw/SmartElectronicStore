908d35437e4a00bc91340ca76e3c55052137a63c
ABINASH KUMAR SHAW <30557422+Abinashshaw@users.noreply.github.com>
2024-12-25 20:34:46 +0530

    Welcome to Smart Electronic Store

---
+++ src/main/java/com/learn/electronic/store/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.learn.electronic.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Smart Electronic Store";
+	}
+	
+}
